889ba2805253c2fdc9d40575b82fbb8bef972fb0
刘元博 <1553592282@qq.com>
2023-03-06 14:00:44 +0800

    导航组件

--- src/components/tab/lm-tab.vue
+++ src/components/tab/lm-tab.vue
@@ -1,10 +1,58 @@
 <template>
-  <router-link to="/recommend">recommend</router-link>
-  <router-link to="/singer">singer</router-link>
-  <router-link to="/top-list">top-list</router-link>
-  <router-link to="/search">search</router-link>
+  <div class="lm-tab">
+    <router-link class="tab-link" v-for="item in tabs" :key="item.path" :to="item.path">
+      <span class="tab-item">
+        {{ item.name }}
+      </span>
+    </router-link>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style scoped lang="scss"></style>
+const tabs = ref([
+  {
+    name: '推荐',
+    path: '/recommend'
+  },
+  {
+    name: '歌手',
+    path: '/singer'
+  },
+  {
+    name: '排行',
+    path: '/top-list'
+  },
+  {
+    name: '搜索',
+    path: '/search'
+  }
+])
+</script>
+
+<style scoped lang="scss">
+.lm-tab {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  font-size: $font-size-medium;
+
+  .tab-link {
+    flex: 1;
+
+    .tab-item {
+      color: $color-text-l;
+      padding-bottom: 5px;
+    }
+
+    &.router-link-active {
+      .tab-item {
+        color: $color-theme;
+        border-bottom: 2px solid $color-theme;
+      }
+    }
+  }
+}
+</style>
